6dc49ac190f0bdd37ba29050055759f422683f0b
Thanhhoacam <tienthanh1782003@gmail.com>
2024-03-13 19:29:24 +0700

    delete company model

--- Bulky.Models/ViewModels/RoleManagmentVM.cs
+++ Bulky.Models/ViewModels/RoleManagmentVM.cs
@@ -9,6 +9,6 @@ namespace BulkyBook.Models.ViewModels {
     public class RoleManagmentVM {
         public ApplicationUser ApplicationUser { get; set; }
         public IEnumerable<SelectListItem> RoleList { get; set; }
-        public IEnumerable<SelectListItem> CompanyList { get; set; }
+       
     }
 }
